0e6687353b09c4db744d02171ec0e9edc3324142
student2 <student@student.com>
2021-02-27 15:22:15 +0200

    releaze

--- js/script.js
+++ js/script.js
@@ -4,4 +4,32 @@ $('.main-content').slick({
     slidesToScroll: 1,
 
     autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
   });
